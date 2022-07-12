5d6dcf21d68f1035a6aa37e18879396728833dba
skydoves <skydoves@naver.com>
2022-07-12 09:40:37 +0900

    Add spotless for auto formatting, lint chechking and license generating

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -14,19 +14,3 @@
  * limitations under the License.
  */
 
-package io.getstream.video.android
-
-import org.junit.Assert.assertEquals
-import org.junit.Test
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-}
