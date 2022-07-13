cb8c026b33c34595ab4423e95f112f1c1b66a8ff
Filip Babic <fbabic96@gmail.com>
2022-07-13 10:18:05 +0200

    Permissions handling

--- app/src/main/kotlin/io/getstream/video/android/app/MainActivity.kt
+++ app/src/main/kotlin/io/getstream/video/android/app/MainActivity.kt
@@ -16,8 +16,18 @@
 
 package io.getstream.video.android.app
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION_CODES.M
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import io.getstream.video.android.app.databinding.ActivityMainBinding
@@ -32,14 +42,112 @@ import kotlinx.coroutines.launch
 import stream.video.SelectEdgeServerRequest
 import stream.video.SelectEdgeServerResponse
 
+
 class MainActivity : AppCompatActivity(), RoomListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var hasInitializedVideo: Boolean = false
+
+    @RequiresApi(M)
+    private val permissionsContract = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        val missing = getMissingPermissions()
+        val deniedCamera = !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+        val deniedMicrophone =
+            !shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)
+
+        when {
+            missing.isNotEmpty() && !deniedCamera && !deniedMicrophone -> requestPermissions(missing)
+            isGranted -> startVideoFlow()
+            deniedCamera || deniedMicrophone -> showPermissionsDialog()
+            else -> {
+                checkPermissions()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= M) {
+            checkPermissions()
+        } else {
+            startVideoFlow()
+        }
+    }
+
+    @RequiresApi(M)
+    private fun checkPermissions() {
+        val missing = getMissingPermissions()
+
+        if (missing.isNotEmpty()) {
+            requestPermissions(missing)
+        } else {
+            startVideoFlow()
+        }
+    }
+
+    @RequiresApi(M)
+    private fun requestPermissions(permissions: Array<out String>) {
+        val deniedCamera = !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+        val deniedMicrophone =
+            !shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO)
+
+        if (!deniedCamera && !deniedMicrophone) {
+            permissionsContract.launch(permissions.first())
+        } else {
+            showPermissionsDialog()
+        }
+    }
+
+    @RequiresApi(M)
+    private fun getMissingPermissions(): Array<out String> {
+        val permissionsToCheck = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
+
+        val missing = permissionsToCheck
+            .map { it to (checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED) }
+            .filter { (_, isGranted) -> !isGranted }
+            .map { it.first }
+
+        return missing.toTypedArray()
+    }
+
+    private fun showPermissionsDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Permissions required to launch the app")
+            .setMessage("Open settings to allow camera and microphone permissions.")
+            .setPositiveButton("Launch settings") { dialog, _ ->
+                startSettings()
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                finish()
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun startSettings() {
+        startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            val uri = Uri.fromParts("package", packageName, null)
+            data = uri
+        })
+    }
+
+    private fun startVideoFlow() {
+        if (hasInitializedVideo) return
 
         val client = VideoApp.videoClient
 
@@ -90,11 +198,16 @@ class MainActivity : AppCompatActivity(), RoomListener {
         // we need to connect both the room to the renderer and the track, to show something
         room.initVideoRenderer(binding.rendererView)
         videoTrack.addRenderer(binding.rendererView)
+        hasInitializedVideo = true
     }
 
     private fun enrichUrl(url: String): String {
         if (url.startsWith("wss://")) return url
 
         return "wss://$url"
+    }
+
+    companion object {
+        private const val CODE_PERMISSIONS = 101
     }
 }
