cbcf715981181d2b51f0f4adfef4c40666e86395
skydoves <skydoves@naver.com>
2022-07-13 09:11:09 +0900

    Formatting codes

--- app/src/main/kotlin/io/getstream/video/android/app/FakeTokenProvider.kt
+++ app/src/main/kotlin/io/getstream/video/android/app/FakeTokenProvider.kt
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.video.android.app
 
 import io.getstream.video.android.token.TokenProvider
 
-class FakeTokenProvider: TokenProvider {
+class FakeTokenProvider : TokenProvider {
 
     override fun provideUserToken(): String {
         return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidG9tbWFzbyJ9.XGkxJKi33fHr3cHyLFc6HRnbPgLuwNHuETWQ2MWzz5c"
