4d85e38e34896120c08f26001fc5fbc475f7f0f4
filbabic <fbabic96@gmail.com>
2022-11-10 10:16:58 +0100

    cleanup, merge conflicts

--- app/src/main/kotlin/io/getstream/video/android/app/ui/outgoing/OutgoingCallActivity.kt
+++ app/src/main/kotlin/io/getstream/video/android/app/ui/outgoing/OutgoingCallActivity.kt
@@ -25,7 +25,9 @@ import androidx.appcompat.app.AppCompatActivity
 import io.getstream.video.android.app.router.StreamRouterImpl
 import io.getstream.video.android.app.videoApp
 import io.getstream.video.android.compose.theme.VideoTheme
-import io.getstream.video.android.compose.ui.components.outcomingcall.OutgoingCallScreen
+import io.getstream.video.android.compose.ui.components.outgoingcall.OutgoingCallScreen
+import io.getstream.video.android.viewmodel.OutgoingCallViewModel
+import io.getstream.video.android.viewmodel.OutgoingCallViewModelFactory
 
 class OutgoingCallActivity : AppCompatActivity() {
 
