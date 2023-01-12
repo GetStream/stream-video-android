6ed5f93cead4c53fb3d98a305d0925c2042d21a9
Filip Babic <fbabic96@gmail.com>
2023-01-12 14:51:55 +0100

    Project structure change, rename and similar

---
+++ examples/chat-with-video/chat-with-video-starter-kit/src/main/java/io/getstream/video/chat_with_video_starter_kit/ui/call/CallActivity.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.chat_with_video_starter_kit.ui.call
+
+import android.content.Context
+import io.getstream.video.android.StreamVideo
+import io.getstream.video.android.compose.ui.AbstractComposeCallActivity
+import io.getstream.video.android.viewmodel.CallViewModelFactory
+import io.getstream.video.chat_with_video_starter_kit.application.chatWithVideoApp
+
+class CallActivity : AbstractComposeCallActivity() {
+
+    /**
+     * Provides the StreamVideo instance through the videoApp.
+     */
+    override fun getStreamVideo(context: Context): StreamVideo =
+        context.chatWithVideoApp.streamVideo
+
+    /**
+     * Provides a custom factory for the ViewModel, that provides fake users for invites.
+     */
+    override fun getCallViewModelFactory(): CallViewModelFactory {
+        return CallViewModelFactory(
+            streamVideo = getStreamVideo(this),
+            permissionManager = getPermissionManager(),
+            usersProvider = chatWithVideoApp.usersLoginProvider
+        )
+    }
+}
---
+++ examples/chat-with-video/chat-with-video-starter-kit/src/main/java/io/getstream/video/chat_with_video_starter_kit/ui/messages/MessagesActivity.kt
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.chat_with_video_starter_kit.ui.messages
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.compose.ui.messages.composer.MessageComposer
+import io.getstream.chat.android.compose.ui.messages.header.MessageListHeader
+import io.getstream.chat.android.compose.ui.messages.list.MessageList
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.compose.viewmodel.messages.MessageComposerViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessageListViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFactory
+import io.getstream.chat.android.uiutils.extension.getDisplayName
+import io.getstream.video.android.utils.Success
+import io.getstream.video.chat_with_video_starter_kit.application.chatWithVideoApp
+import io.getstream.video.chat_with_video_starter_kit.R
+import kotlinx.coroutines.launch
+import java.util.*
+
+class MessagesActivity : ComponentActivity() {
+
+    private val factory by lazy {
+        MessagesViewModelFactory(
+            context = this,
+            channelId = intent.getStringExtra(KEY_CHANNEL_ID)!!
+        )
+    }
+
+    private val messageListViewModel by viewModels<MessageListViewModel> { factory }
+
+    private val composerViewModel by viewModels<MessageComposerViewModel> { factory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ChatTheme(attachmentFactories = chatWithVideoApp.attachmentFactories) {
+                val channelState = messageListViewModel.channel
+                val currentUser by messageListViewModel.user.collectAsState()
+
+                Scaffold(
+                    topBar = {
+                        MessageListHeader(
+                            channel = channelState,
+                            currentUser = currentUser,
+                            trailingContent = { CallButton() },
+                            onBackPressed = { finish() }
+                        )
+                    },
+                    bottomBar = { MessageComposer(viewModel = composerViewModel) },
+                    content = { paddingValues ->
+                        MessageList(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(ChatTheme.colors.appBackground)
+                                .padding(paddingValues),
+                            viewModel = messageListViewModel,
+                        )
+                    }
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun CallButton() {
+        Icon(
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .clickable { startCall() },
+            imageVector = Icons.Default.Call,
+            contentDescription = null,
+            tint = ChatTheme.colors.textHighEmphasis
+        )
+    }
+
+    private fun startCall() {
+        val videoClient = chatWithVideoApp.streamVideo
+
+        lifecycleScope.launch {
+            val callId = UUID.randomUUID().toString()
+
+            val createCallResult = videoClient.createCall(
+                id = callId,
+                type = "default",
+                ringing = false,
+                participantIds = emptyList()
+            )
+
+            if (createCallResult is Success) {
+                val data = createCallResult.data
+
+                val customAttachment = Attachment(
+                    type = "custom",
+                    authorName = videoClient.getUser().name,
+                    extraData = mutableMapOf(
+                        "callCid" to data.cid,
+                        "members" to data.users.map { it.value.name },
+                        "callName" to messageListViewModel.channel.getDisplayName(
+                            context = this@MessagesActivity,
+                            fallback = R.string.call_name_placeholder
+                        )
+                    )
+                )
+
+                val newMessage = composerViewModel.buildNewMessage("", listOf(customAttachment))
+                composerViewModel.sendMessage(newMessage)
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY_CHANNEL_ID = "channel_id"
+        fun getIntent(context: Context, channelId: String): Intent {
+            return Intent(context, MessagesActivity::class.java).apply {
+                putExtra(KEY_CHANNEL_ID, channelId)
+            }
+        }
+    }
+}
