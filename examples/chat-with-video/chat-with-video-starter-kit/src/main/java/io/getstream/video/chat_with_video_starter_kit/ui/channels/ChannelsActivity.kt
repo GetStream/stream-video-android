43a6a6e81cda1f86383c59fcb6f1707b97550bb5
Filip Babic <fbabic96@gmail.com>
2023-01-12 15:12:43 +0100

    Channels strip down

--- examples/chat-with-video/chat-with-video-starter-kit/src/main/java/io/getstream/video/chat_with_video_starter_kit/ui/channels/ChannelsActivity.kt
+++ examples/chat-with-video/chat-with-video-starter-kit/src/main/java/io/getstream/video/chat_with_video_starter_kit/ui/channels/ChannelsActivity.kt
@@ -21,39 +21,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Logout
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import io.getstream.chat.android.client.api.models.querysort.QuerySortByField
 import io.getstream.chat.android.client.models.Channel
-import io.getstream.chat.android.compose.ui.channels.header.ChannelListHeader
-import io.getstream.chat.android.compose.ui.channels.list.ChannelList
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
-import io.getstream.chat.android.compose.viewmodel.channels.ChannelListViewModel
-import io.getstream.chat.android.compose.viewmodel.channels.ChannelViewModelFactory
 import io.getstream.video.chat_with_video_starter_kit.application.chatWithVideoApp
 import io.getstream.video.chat_with_video_starter_kit.ui.messages.MessagesActivity
 
 class ChannelsActivity : ComponentActivity() {
-
-    private val factory by lazy {
-        ChannelViewModelFactory(
-            chatClient = chatWithVideoApp.chatClient,
-            querySort = QuerySortByField.descByName("last_updated"),
-            filters = null
-        )
-    }
-
-    private val channelListViewModel by viewModels<ChannelListViewModel> { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,34 +41,6 @@ class ChannelsActivity : ComponentActivity() {
                         .fillMaxSize()
                         .background(color = ChatTheme.colors.appBackground)
                 ) {
-                    val user by channelListViewModel.user.collectAsState()
-
-                    ChannelListHeader(
-                        modifier = Modifier.fillMaxWidth(),
-                        title = "Chat with Video Sample",
-                        currentUser = user,
-                        trailingContent = {
-                            IconButton(
-                                onClick = { logOut() },
-                                content = {
-                                    Icon(
-                                        imageVector = Icons.Default.Logout,
-                                        contentDescription = null,
-                                        tint = ChatTheme.colors.errorAccent
-                                    )
-                                }
-                            )
-                        }
-                    )
-
-                    ChannelList(
-                        modifier = Modifier.fillMaxSize(),
-                        viewModel = channelListViewModel,
-                        onChannelLongClick = {},
-                        onChannelClick = { channel ->
-                            openMessages(channel)
-                        }
-                    )
                 }
             }
         }
