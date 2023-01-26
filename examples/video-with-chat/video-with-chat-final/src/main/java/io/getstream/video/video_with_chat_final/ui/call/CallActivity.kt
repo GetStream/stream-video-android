68013a5108534cb535f1a3d6240e4879d8dcb079
Filip Babic <fbabic96@gmail.com>
2023-01-26 12:00:42 +0100

    Move parts from starter to final

--- examples/video-with-chat/video-with-chat-final/src/main/java/io/getstream/video/video_with_chat_final/ui/call/CallActivity.kt
+++ examples/video-with-chat/video-with-chat-final/src/main/java/io/getstream/video/video_with_chat_final/ui/call/CallActivity.kt
@@ -17,10 +17,37 @@
 package io.getstream.video.video_with_chat_final.ui.call
 
 import android.content.Context
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import io.getstream.video.android.StreamVideo
+import io.getstream.video.android.call.state.CallAction
+import io.getstream.video.android.call.state.CallMediaState
+import io.getstream.video.android.call.state.CustomAction
+import io.getstream.video.android.compose.state.ui.call.CallControlAction
+import io.getstream.video.android.compose.theme.VideoTheme
 import io.getstream.video.android.compose.ui.AbstractComposeCallActivity
+import io.getstream.video.android.compose.ui.components.call.CallContent
+import io.getstream.video.android.compose.ui.components.call.controls.CallControls
+import io.getstream.video.android.compose.ui.components.call.controls.buildDefaultCallControlActions
+import io.getstream.video.android.model.state.StreamCallState
 import io.getstream.video.android.viewmodel.CallViewModelFactory
+import io.getstream.video.video_with_chat_final.R
 import io.getstream.video.video_with_chat_final.application.videoWithChatApp
+import kotlinx.coroutines.flow.emptyFlow
 
 class CallActivity : AbstractComposeCallActivity() {
 
@@ -29,6 +56,8 @@ class CallActivity : AbstractComposeCallActivity() {
      */
     override fun getStreamVideo(context: Context): StreamVideo =
         context.videoWithChatApp.streamVideo
+
+    private val isShowingChatState = mutableStateOf(false)
 
     /**
      * Provides a custom factory for the ViewModel, that provides fake users for invites.
@@ -39,5 +68,95 @@ class CallActivity : AbstractComposeCallActivity() {
             permissionManager = getPermissionManager(),
             usersProvider = videoWithChatApp.usersLoginProvider
         )
+    }
+
+    override fun buildContent(): @Composable () -> Unit {
+        return {
+            VideoTheme {
+                val isShowingChat by remember { isShowingChatState }
+
+                if (isShowingChat) {
+                    ChatContent()
+                } else {
+                    CallContent(
+                        modifier = Modifier.background(color = VideoTheme.colors.appBackground),
+                        viewModel = callViewModel,
+                        onCallAction = ::handleCallAction,
+                        onBackPressed = ::handleBackPressed,
+                        pictureInPictureContent = { PictureInPictureContent(call = it) },
+                        callControlsContent = { CustomCallControlsContent() }
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun ChatContent() {
+        // TODO - build chat UI and logic for loading the data
+    }
+
+    @Composable
+    private fun CustomCallControlsContent() {
+        val call by callViewModel.callState.collectAsState(initial = null)
+        val callMediaState by callViewModel.callMediaState.collectAsState(initial = CallMediaState())
+
+        val screenShareSessionsState = call?.screenSharingSessions ?: emptyFlow()
+        val state by screenShareSessionsState.collectAsState(initial = emptyList())
+
+        val streamCallState by callViewModel.streamCallState.collectAsState()
+
+        val isScreenSharing = state.isNotEmpty()
+
+        val orientation = LocalConfiguration.current.orientation
+
+        val modifier = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Modifier
+                .fillMaxHeight()
+                .width(VideoTheme.dimens.landscapeCallControlsSheetWidth)
+        } else {
+            Modifier
+                .fillMaxWidth()
+                .height(VideoTheme.dimens.callControlsSheetHeight)
+        }
+
+        val currentCallState = (streamCallState as? StreamCallState.Connected)
+        val defaultActions = buildDefaultCallControlActions(callMediaState = callMediaState)
+
+        val actions = if (currentCallState is StreamCallState.Connected) {
+            val customAction = CustomAction(
+                data = mapOf(
+                    "channelCid" to currentCallState.callGuid.cid
+                )
+            )
+
+            val customCallControlAction = CallControlAction(
+                actionBackgroundTint = Color.White,
+                icon = painterResource(id = R.drawable.ic_chat_bubble),
+                iconTint = Color.DarkGray,
+                callAction = customAction,
+                description = stringResource(R.string.open_chat)
+            )
+
+            listOf(customCallControlAction) + defaultActions
+        } else {
+            defaultActions
+        }
+
+        CallControls(
+            modifier = modifier,
+            callMediaState = callMediaState,
+            isScreenSharing = isScreenSharing,
+            onCallAction = ::handleCallAction,
+            actions = actions
+        )
+    }
+
+    override fun handleCallAction(action: CallAction) {
+        if (action is CustomAction) {
+            isShowingChatState.value = true
+        } else {
+            super.handleCallAction(action)
+        }
     }
 }
