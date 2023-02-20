fd541ada68249a131579bf5bc86443c37a659256
skydoves <skydoves@naver.com>
2023-02-20 10:27:35 +0900

    Remove Jitpack setup and user guidelines

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    maven(url = "https://jitpack.io")
     maven(url = "https://plugins.gradle.org/m2/")
   }
 }
