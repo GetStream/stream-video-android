87fe2adb414f3b4b77e5773bfdd0482b0a385fdc
Thierry Schellenbach <thierryschellenbach@gmail.com>
2023-04-08 13:35:24 -0600

    added android test, doesn't work yet

--- stream-video-android-core/build.gradle.kts
+++ stream-video-android-core/build.gradle.kts
@@ -129,6 +129,8 @@ dependencies {
     implementation(libs.moshi)
     implementation(libs.moshi.kotlin)
     implementation(libs.moshi.adapters)
+    implementation(libs.desugar)
+
 
     // Stream
     implementation(libs.stream.log)
@@ -136,7 +138,6 @@ dependencies {
     implementation(libs.stream.push.delegate)
 
     // unit test
-    testImplementation(libs.stream.webrtc)
     testImplementation(libs.junit)
     testImplementation(libs.truth)
     testImplementation(libs.mockk)
@@ -147,4 +148,11 @@ dependencies {
     testImplementation(libs.kotlin.test.junit)
 
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.truth)
+    androidTestImplementation(libs.mockk)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.kotlin.test.junit)
 }
