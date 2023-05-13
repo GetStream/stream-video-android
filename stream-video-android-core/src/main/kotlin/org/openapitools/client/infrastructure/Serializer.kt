7f12227744406a64bb3807a4723c1541abda6f7d
Thierry Schellenbach <thierryschellenbach@gmail.com>
2023-05-13 15:42:22 -0600

    update to latest openapi spec

--- stream-video-android-core/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
+++ stream-video-android-core/src/main/kotlin/org/openapitools/client/infrastructure/Serializer.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2014-2023 Stream.io Inc. All rights reserved.
- *
- * Licensed under the Stream License;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.openapitools.client.infrastructure
 
 import com.squareup.moshi.Moshi
@@ -31,16 +15,6 @@ object Serializer {
         .add(URIAdapter())
         .addLast(KotlinJsonAdapterFactory())
         .add(VideoEventAdapter())
-        .add(org.openapitools.client.models.APIError.Code.CodeAdapter())
-        .add(org.openapitools.client.models.CreateDeviceRequest.PushProvider.PushProviderAdapter())
-        .add(org.openapitools.client.models.DeviceFieldsRequest.PushProvider.PushProviderAdapter())
-        .add(org.openapitools.client.models.OwnCapability.OwnCapabilityAdapter())
-        .add(org.openapitools.client.models.RecordSettings.Mode.ModeAdapter())
-        .add(org.openapitools.client.models.RecordSettings.Quality.QualityAdapter())
-        .add(org.openapitools.client.models.RecordSettingsRequest.Mode.ModeAdapter())
-        .add(org.openapitools.client.models.RecordSettingsRequest.Quality.QualityAdapter())
-        .add(org.openapitools.client.models.TranscriptionSettings.Mode.ModeAdapter())
-        .add(org.openapitools.client.models.TranscriptionSettingsRequest.Mode.ModeAdapter())
         .add(BigDecimalAdapter())
         .add(BigIntegerAdapter())
 
