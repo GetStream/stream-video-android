d496aa1730399a0632709edefa6841090fba4188
skydoves <skydoves@naver.com>
2023-05-22 06:33:52 -0400

    Add zoomable parameters to screensharing renderers

--- stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/call/renderer/CallVideoRenderer.kt
+++ stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/call/renderer/CallVideoRenderer.kt
@@ -19,7 +19,6 @@ package io.getstream.video.android.compose.ui.components.call.renderer
 import android.view.View
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
