feca2b7936525423b4d9a2e02052a3f1ca0c6198
Thierry Schellenbach <thierryschellenbach@gmail.com>
2023-06-06 14:53:07 +0300

    increase patch version to 4

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,7 +6,7 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 0
     const val minorVersion = 0
-    const val patchVersion = 2
+    const val patchVersion = 4
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 1
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
