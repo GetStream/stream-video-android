cd682aa7404bccf117f5a338504a2b235c71a331
skydoves <skydoves@naver.com>
2023-06-20 10:40:15 +0900

    Update docs for screensharing

--- stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/call/renderer/ParticipantsScreenSharing.kt
+++ stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/call/renderer/ParticipantsScreenSharing.kt
@@ -36,6 +36,7 @@ import io.getstream.video.android.core.model.ScreenSharingSession
  * @param call The call that contains all the participants state and tracks.
  * @param session The screen sharing session which is active.
  * @param modifier Modifier for styling.
+ * @param isZoomable Decide to this screensharing video renderer is zoomable or not.
  * @param style Defined properties for styling a single video call track.
  * @param videoRenderer A single video renderer renders each individual participant.
  */
