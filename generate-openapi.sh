1fb6e4867013c95638630c2148556ab1f78e9fa7
Tommaso Barbugli <tbarbugli@gmail.com>
2023-07-07 14:24:54 +0200

    improve generate-openapi.sh and make it easier to use local yaml

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -7,90 +7,27 @@ PROTOCOL_ROOT="$BASEDIR/protocol"
 PROJECT_ROOT=$(pwd)
 GENERATED_CODE_ROOT="$BASEDIR/generated"
 PROTOCOL_PULL_LATEST="n"
+OPENAPI_SPEC_PATH="https://raw.githubusercontent.com/GetStream/protocol/main/openapi/video-openapi-clientside.yaml"
+DOCKER_EXTRA_MOUNT=""
 
-echo "Enter path to protocol root. If the project is missing will clone the repo to the path, default: ($PROTOCOL_ROOT):"
-read -r PROTOCOL_ROOT_TEMP
-if [ -n "$PROTOCOL_ROOT_TEMP" ]; then
-  PROTOCOL_ROOT=$PROTOCOL_ROOT_TEMP
-fi
-
-echo "Enter path to project root, default: ($PROJECT_ROOT):"
-read -r PROJECT_ROOT_TEMP
-if [ -n "$PROJECT_ROOT_TEMP" ]; then
-  PROJECT_ROOT=$PROJECT_ROOT_TEMP
-fi
-
-echo "Enter path to generated root, default: ($GENERATED_CODE_ROOT):"
-read -r GENERATED_CODE_ROOT_TEMP
-if [ -n "$GENERATED_CODE_ROOT_TEMP" ]; then
-  GENERATED_CODE_ROOT=$GENERATED_CODE_ROOT_TEMP
-fi
-
-echo "Do you want to pull the latest protocol if exists, default:($PROTOCOL_PULL_LATEST); (y/n):"
-read -r PROTOCOL_PULL_LATEST_TEMP
-if [ -n "$PROTOCOL_PULL_LATEST_TEMP" ]; then
-  PROTOCOL_PULL_LATEST=$PROTOCOL_PULL_LATEST_TEMP
-fi
-
-echo Generating OpenAPI spec
-
-if [ -d "$PROTOCOL_ROOT" ]; then
-  if [ "$PROTOCOL_PULL_LATEST" = "y" ]; then
-    git -C "$PROTOCOL_ROOT" pull
-  fi
-
-else
-  git clone git@github.com:GetStream/protocol.git "$PROTOCOL_ROOT"
+if [ ! -z $1 ]
+then
+    DOCKER_EXTRA_MOUNT="-v $(dirname $1):/manifest"
+    OPENAPI_SPEC_PATH="/manifest/$(basename $1)"
 fi
 
 CLIENT_ROOT="$GENERATED_CODE_ROOT/src/main/kotlin/org/openapitools/client"
 rm -rf "${CLIENT_ROOT}"
 
-# you can use this to use openapi templates from your laptop (openapi-generator needs to be in ~/src)
-#java -jar ~/src/openapi-generator/modules/openapi-generator-cli/target/openapi-generator-cli.jar generate \
-#   -i ~/src/protocol/openapi/video-openapi.yaml --additional-properties=library=jvm-retrofit2,useCoroutines,dateLibrary=threetenbp \
-#   -t ~/src/openapi-generator/modules/openapi-generator/src/main/resources/kotlin-client/ \
-#   -g kotlin \
-#   -o "${GENERATED_CODE_ROOT}"
+docker pull ghcr.io/getstream/openapi-generator:master
 
-if [[ -n "${LOCAL_ENV}" ]]; then
-  OPENAPI_GENERATOR="${OPENAPI_GENERATOR:-ghcr.io/getstream/openapi-generator:master}"
-
-  # Local environment needs an additional mount for the newly generated openapi
-  # spec. We assume that you have cloned the GetStream/protocol repository and
-  # the file is located in "${PROTOCOL_ROOT}/openapi/video-openapi.yaml"
-  #
-  # This mode is used when you want to check the generated Kotlin code from the yet
-  # to be merged changes to the OpenAPI spec (YAML file) as a result of Coordinator
-  # code changes. This is needed only in advanced use cases.
-  if [[ -n "${OPENAPI_ROOT_USE_COORDINATOR_REPO}" ]]; then
-    OPENAPI_CONFIG_ROOT="${BASEDIR}/chat/releases/"
-    echo "using OpenAPI spec from the local coordinator repository (chat): ${OPENAPI_CONFIG_ROOT}"
-  else
-    OPENAPI_CONFIG_ROOT="${PROTOCOL_ROOT}/openapi/"
-    echo "using OpenAPI spec from the local protocol repository: ${OPENAPI_CONFIG_ROOT}"
-  fi
-
-  docker run --rm \
-    -v "${GENERATED_CODE_ROOT}:/local" \
-    -v "${OPENAPI_CONFIG_ROOT}:/config" \
-    "${OPENAPI_GENERATOR}" generate \
-    -i "/config/video-openapi.yaml" \
-    --additional-properties=library=jvm-retrofit2,useCoroutines,dateLibrary=threetenbp \
-    -g kotlin \
-    -o /local
-
-else
-  # This is the default used in CI/CD pipelines. Here, we pull the OpenAPI spec
-  # from the protocol repository. Most of the times, this is what you would be doing.
-  docker run --rm \
-    -v "${GENERATED_CODE_ROOT}:/local" \
-    ghcr.io/getstream/openapi-generator:master generate \
-    -i https://raw.githubusercontent.com/GetStream/protocol/main/openapi/video-openapi-clientside.yaml \
-    --additional-properties=library=jvm-retrofit2,useCoroutines,dateLibrary=threetenbp \
-    -g kotlin \
-    -o /local
-fi
+docker run --rm \
+  -v "${GENERATED_CODE_ROOT}:/local" ${DOCKER_EXTRA_MOUNT} \
+  ghcr.io/getstream/openapi-generator:master generate \
+  -i "$OPENAPI_SPEC_PATH" \
+  --additional-properties=library=jvm-retrofit2,useCoroutines,dateLibrary=threetenbp \
+  -g kotlin \
+  -o /local
 
 # delete all files in the target path of openapi to make sure we do not leave legacy code around
 rm -rf "${PROJECT_ROOT}/stream-video-android-core/src/main/kotlin/org/openapitools/client"
