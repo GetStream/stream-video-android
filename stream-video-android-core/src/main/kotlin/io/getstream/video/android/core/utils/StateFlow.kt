f1ed957669c5678fd9a8265651818194acf012f6
Daniel Novak <daniel@danielnovak.sk>
2023-07-24 13:37:23 +0200

    Fix mapState extension

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/utils/StateFlow.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/utils/StateFlow.kt
@@ -18,37 +18,51 @@ package io.getstream.video.android.core.utils
 
 import io.getstream.video.android.core.dispatchers.DispatcherProvider
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
-// TODO: review how the scope interacts with the viewmodel
-// https://proandroiddev.com/clean-stateflow-transformations-in-kotlin-608f4c7de5ab
-// probably makes sense to pass down a call scope and cancel that all at once
+/**
+ * Does not produce the same value in a raw, so respect "distinct until changed emissions"
+ * https://github.com/Kotlin/kotlinx.coroutines/issues/2631#issuecomment-870565860
+ * */
+class DerivedStateFlow<T>(
+    private val getValue: () -> T,
+    private val flow: Flow<T>
+) : StateFlow<T> {
 
-fun <T, K> StateFlow<T>.mapState(
-    scope: CoroutineScope = CoroutineScope(context = DispatcherProvider.IO),
-    transform: (data: T) -> K
-): StateFlow<K> {
-    return mapLatest {
-        transform(it)
+    override val replayCache: List<T>
+        get() = listOf(value)
+
+    override val value: T
+        get() = getValue()
+
+    @InternalCoroutinesApi
+    override suspend fun collect(collector: FlowCollector<T>): Nothing {
+        coroutineScope { flow.distinctUntilChanged().stateIn(this).collect(collector) }
     }
-        .stateIn(scope, SharingStarted.Eagerly, transform(value))
 }
 
-fun <T, K> StateFlow<T>.mapState(
-    scope: CoroutineScope,
-    initialValue: K,
-    transform: suspend (data: T) -> K
-): StateFlow<K> {
-    return mapLatest {
-        transform(it)
-    }
-        .stateIn(scope, SharingStarted.Eagerly, initialValue)
+fun <T1, R> StateFlow<T1>.mapState(transform: (a: T1) -> R): StateFlow<R> {
+    return DerivedStateFlow(
+        getValue = { transform(this.value) },
+        flow = this.map { a -> transform(a) }
+    )
 }
 
+fun <T1, T2, R> combineStates(flow: StateFlow<T1>, flow2: StateFlow<T2>, transform: (a: T1, b: T2) -> R): StateFlow<R> {
+    return DerivedStateFlow(
+        getValue = { transform(flow.value, flow2.value) },
+        flow = combine(flow, flow2) { a, b -> transform(a, b) }
+    )
+}
 @JvmSynthetic
 internal fun <T> Flow<T>.asStateFlow(
     initialValue: T,
