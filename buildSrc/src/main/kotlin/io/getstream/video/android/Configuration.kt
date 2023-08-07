8bf73aba94cb6d0ec4d1f7b2aa33bdd384549b36
skydoves <skydoves@naver.com>
2023-08-07 10:38:04 +0900

    Prepare to release 0.2.3

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,9 +6,9 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 0
     const val minorVersion = 2
-    const val patchVersion = 2
+    const val patchVersion = 3
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 4
+    const val versionCode = 5
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
 }
