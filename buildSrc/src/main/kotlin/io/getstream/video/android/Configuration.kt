323973b84c5daf6f0c84ca1eaaef8837a1bc8adb
Jaewoong Eum <skydoves2@gmail.com>
2023-08-07 11:49:32 +0900

    Prepare to release 0.3.0 (#740)

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -5,10 +5,10 @@ object Configuration {
     const val targetSdk = 33
     const val minSdk = 24
     const val majorVersion = 0
-    const val minorVersion = 2
-    const val patchVersion = 2
+    const val minorVersion = 3
+    const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 4
+    const val versionCode = 5
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
 }
