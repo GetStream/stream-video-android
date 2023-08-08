2c547b117eb2b019444d384659e7f83508e01621
Jaewoong Eum <skydoves2@gmail.com>
2023-08-08 09:04:36 +0900

    Prepare to release dogfooding 1.0.1 (#743)

--- dogfooding/build.gradle.kts
+++ dogfooding/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "io.getstream.video.android"
         minSdk = Configuration.minSdk
         targetSdk = Configuration.targetSdk
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
