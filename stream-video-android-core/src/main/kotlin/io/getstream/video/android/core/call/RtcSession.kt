ad39d0650b0e33e00abea59e44ad1845da488115
skydoves <skydoves@naver.com>
2023-10-11 10:36:40 +0900

    Emit updateMuteState request when it's only succeed

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
@@ -21,6 +21,7 @@ import io.getstream.log.taggedLogger
 import io.getstream.result.Result
 import io.getstream.result.Result.Failure
 import io.getstream.result.Result.Success
+import io.getstream.result.onSuccessSuspend
 import io.getstream.video.android.core.Call
 import io.getstream.video.android.core.DeviceStatus
 import io.getstream.video.android.core.MediaManagerImpl
@@ -707,7 +708,7 @@ public class RtcSession internal constructor(
      * -- error isn't permanent, SFU didn't change, the mute/publish state didn't change
      * -- we cap at 30 retries to prevent endless loops
      */
-    fun setMuteState(isEnabled: Boolean, trackType: TrackType) {
+    private fun setMuteState(isEnabled: Boolean, trackType: TrackType) {
         logger.d { "[setPublishState] #sfu; $trackType isEnabled: $isEnabled" }
 
         // update the local copy
@@ -731,7 +732,7 @@ public class RtcSession internal constructor(
                     },
                 )
                 val result = updateMuteState(request)
-                emit(result.getOrThrow())
+                result.onSuccessSuspend { emit(result.getOrThrow()) }
             }.flowOn(DispatcherProvider.IO).retryWhen { cause, attempt ->
                 val sameValue = new == muteState.value
                 val sameSfu = currentSfu == sfuUrl
