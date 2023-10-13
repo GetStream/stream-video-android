d4da528e742aa2114baa9c6d33ee399d9145859d
skydoves <skydoves@naver.com>
2023-10-13 10:13:58 +0900

    Implement LiveBadge composable

--- stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/livestream/LivestreamPlayerOverlay.kt
+++ stream-video-android-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/livestream/LivestreamPlayerOverlay.kt
@@ -16,25 +16,101 @@
 
 package io.getstream.video.android.compose.ui.components.livestream
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.getstream.video.android.compose.theme.VideoTheme
 import io.getstream.video.android.core.Call
+import io.getstream.video.android.mock.StreamMockUtils
+import io.getstream.video.android.mock.mockCall
 
 @Composable
 public fun BoxScope.LivestreamPlayerOverlay(call: Call) {
     val duration by call.state.duration.collectAsState()
-    val totalParticipants by call.state.totalParticipants.collectAsState()
 
     Box(
         modifier = Modifier
             .align(Alignment.BottomCenter)
             .fillMaxWidth(),
     ) {
+        LiveBadge(call = call)
+    }
+}
+
+@Composable
+private fun BoxScope.LiveBadge(call: Call) {
+    val totalParticipants by call.state.totalParticipants.collectAsState()
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .align(Alignment.BottomCenter),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            modifier = Modifier
+                .background(
+                    color = VideoTheme.colors.primaryAccent,
+                    shape = RoundedCornerShape(6.dp),
+                )
+                .padding(horizontal = 16.dp, vertical = 4.dp),
+            text = stringResource(
+                id = io.getstream.video.android.ui.common.R.string.stream_video_live,
+            ),
+            color = Color.White,
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Image(
+            modifier = Modifier.size(22.dp),
+            painter = painterResource(
+                id = io.getstream.video.android.ui.common.R.drawable.stream_video_ic_live,
+            ),
+            contentDescription = stringResource(
+                id = io.getstream.video.android.ui.common.R.string.stream_video_live,
+            ),
+        )
+
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = totalParticipants.toString(),
+            color = Color.White,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LivestreamPlayerOverlayPreview() {
+    StreamMockUtils.initializeStreamVideo(LocalContext.current)
+    VideoTheme {
+        Box {
+            LivestreamPlayerOverlay(call = mockCall)
+        }
     }
 }
