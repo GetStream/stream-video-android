341a8323c42168112c741b4567dc0d4dab54e72e
Liviu Timar <liviu.timar@autoliv.com>
2023-10-24 13:22:28 +0300

    Add silent sign in in GoogleAccountRepository for token refresh

--- dogfooding/src/main/kotlin/io/getstream/video/android/data/repositories/GoogleAccountRepository.kt
+++ dogfooding/src/main/kotlin/io/getstream/video/android/data/repositories/GoogleAccountRepository.kt
@@ -1,9 +1,11 @@
 package io.getstream.video.android.data.repositories
 
 import android.content.Context
+import android.util.Log
 import com.google.android.gms.auth.GoogleAuthUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapter
@@ -12,6 +14,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import io.getstream.video.android.data.dto.GetGoogleAccountsResponseDto
 import io.getstream.video.android.data.dto.asDomainModel
 import io.getstream.video.android.models.GoogleAccount
+import io.getstream.video.android.util.GoogleSignInHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.HttpUrl.Companion.toHttpUrl
@@ -29,24 +32,53 @@ class GoogleAccountRepository @Inject constructor(
         val sources = "sources=DIRECTORY_SOURCE_TYPE_DOMAIN_PROFILE"
         val pageSize = "pageSize=1000"
 
-        return GoogleSignIn.getLastSignedInAccount(context)?.let { account ->
-            withContext(Dispatchers.IO) {
-                getAccessToken(account)?.let { accessToken ->
-                    val urlString = "$baseUrl?access_token=$accessToken&$readMask&$sources&$pageSize"
-                    val request = buildRequest(urlString)
-                    val okHttpClient = buildOkHttpClient()
-                    var responseBody: String?
+        return if (silentSignIn()) {
+            GoogleSignIn.getLastSignedInAccount(context)?.let { account ->
+                withContext(Dispatchers.IO) {
+                    getAccessToken(account)?.let { accessToken ->
+                        val urlString = "$baseUrl?access_token=$accessToken&$readMask&$sources&$pageSize"
+                        val request = buildRequest(urlString)
+                        val okHttpClient = buildOkHttpClient()
+                        var responseBody: String?
 
-                    okHttpClient.newCall(request).execute().let { response ->
-                        if (response.isSuccessful) {
-                            responseBody = response.body?.string()
-                            responseBody?.let { parseUserListJson(it) }
-                        } else {
-                            null
+                        okHttpClient.newCall(request).execute().let { response ->
+                            if (response.isSuccessful) {
+                                responseBody = response.body?.string()
+                                responseBody?.let { parseUserListJson(it) }
+                            } else {
+                                null
+                            }
                         }
                     }
                 }
             }
+        } else {
+            null
+        }
+    }
+
+    private fun silentSignIn(): Boolean { // Used to refresh token
+        val gsc = GoogleSignInHelper.getGoogleSignInClient(context)
+        val task = gsc.silentSignIn()
+
+        // Below code needed for debugging silent sign-in failures
+        if (task.isSuccessful) {
+            Log.d("Google Silent Sign In", "Successful")
+            return true
+        } else {
+            task.addOnCompleteListener {
+                try {
+                    val signInAccount = task.getResult(ApiException::class.java)
+                    Log.d("Google Silent Sign In", signInAccount.email.toString())
+                } catch (apiException: ApiException) {
+                    // You can get from apiException.getStatusCode() the detailed error code
+                    // e.g. GoogleSignInStatusCodes.SIGN_IN_REQUIRED means user needs to take
+                    // explicit action to finish sign-in;
+                    // Please refer to GoogleSignInStatusCodes Javadoc for details
+                    Log.d("Google Silent Sign In", apiException.statusCode.toString())
+                }
+            }
+            return false
         }
     }
 
