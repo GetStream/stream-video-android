51afc568d9b266ebc31f063ecb0f4869cb7f4963
skydoves <skydoves@naver.com>
2023-10-25 14:31:49 +0900

    Imlpement token utils and unit tests

---
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/utils/TokenUtils.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014-2023 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.android.core.utils
+
+import android.util.Base64
+import io.getstream.log.taggedLogger
+import org.json.JSONException
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
+
+internal object TokenUtils {
+
+    val logger by taggedLogger("Chat:TokenUtils")
+
+    fun getUserId(token: String): String = try {
+        JSONObject(
+            token
+                .takeIf { it.contains(".") }
+                ?.split(".")
+                ?.getOrNull(1)
+                ?.let {
+                    String(
+                        Base64.decode(
+                            it.toByteArray(StandardCharsets.UTF_8),
+                            Base64.NO_WRAP,
+                        ),
+                    )
+                }
+                ?: "",
+        ).optString("user_id")
+    } catch (e: JSONException) {
+        logger.e(e) { "Unable to obtain userId from JWT Token Payload" }
+        ""
+    } catch (e: IllegalArgumentException) {
+        logger.e(e) { "Unable to obtain userId from JWT Token Payload" }
+        ""
+    }
+
+    /**
+     * Generate a developer token that can be used to connect users while the app is using a development environment.
+     *
+     * @param userId the desired id of the user to be connected.
+     */
+    fun devToken(userId: String): String {
+        require(userId.isNotEmpty()) { "User id must not be empty" }
+        val header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" //  {"alg": "HS256", "typ": "JWT"}
+        val devSignature = "devtoken"
+        val payload: String =
+            Base64.encodeToString(
+                "{\"user_id\":\"$userId\"}".toByteArray(StandardCharsets.UTF_8),
+                Base64.NO_WRAP,
+            )
+        return "$header.$payload.$devSignature"
+    }
+}
