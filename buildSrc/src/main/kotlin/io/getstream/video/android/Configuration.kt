0e318c2f5423dd9a1fb2d63dcd6319b664caf1f2
skydoves <skydoves@naver.com>
2023-10-31 14:42:59 +0900

    Prepare for release 0.4.2

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,9 +6,9 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 0
     const val minorVersion = 4
-    const val patchVersion = 1
+    const val patchVersion = 2
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 11
+    const val versionCode = 12
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
     const val streamVideoCallGooglePlayVersion = "1.0.2"
