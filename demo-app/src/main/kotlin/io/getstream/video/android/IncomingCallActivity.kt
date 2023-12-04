a4dc4f00c098822cea9f286df9c89f17a55f155d
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2023-12-04 19:14:14 +0100

    Remove redundant call checks from IncomingCallActivity

--- demo-app/src/main/kotlin/io/getstream/video/android/IncomingCallActivity.kt
+++ demo-app/src/main/kotlin/io/getstream/video/android/IncomingCallActivity.kt
@@ -25,12 +25,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import io.getstream.result.Result
 import io.getstream.video.android.compose.theme.VideoTheme
 import io.getstream.video.android.compose.ui.components.call.activecall.CallContent
 import io.getstream.video.android.compose.ui.components.call.ringing.RingingCallContent
+import io.getstream.video.android.core.Call
 import io.getstream.video.android.core.StreamVideo
 import io.getstream.video.android.core.call.state.AcceptCall
 import io.getstream.video.android.core.call.state.CallAction
@@ -53,55 +56,13 @@ class IncomingCallActivity : ComponentActivity() {
     @Inject
     lateinit var dataStore: StreamUserDataStore
 
+    private var callState: Call? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // release the lock, turn on screen, and keep the device awake.
-        showWhenLockedAndTurnScreenOn()
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
-        val callId = intent.streamCallId(NotificationHandler.INTENT_EXTRA_CALL_CID)!!
-        val streamVideo = StreamVideo.instance()
-
-        lifecycleScope.launch {
-            // Not necessary if you initialise the SDK in Application.onCreate()
-            StreamVideoInitHelper.loadSdk(dataStore = dataStore)
-            val call = streamVideo.call(callId.type, callId.id)
-
-            // We also check if savedInstanceState is null to prevent duplicate calls when activity
-            // is recreated (e.g. when entering PiP mode)
-            // TODO: AAP This is the same logic as in CallActivity, should be merged
-            if (NotificationHandler.ACTION_ACCEPT_CALL == intent.action && savedInstanceState == null) {
-                call.accept()
-                val activeCall = streamVideo.state.activeCall.value
-                val activeOrNewCall = if (activeCall != null) {
-                    if (activeCall.id != callId.id) {
-                        Log.w("CallActivity", "A call with id: ${callId.cid} existed. Leaving.")
-                        // If the call id is different leave the previous call
-                        activeCall.leave()
-                        // Return a new call
-                        streamVideo.call(type = callId.type, id = callId.id)
-                    } else {
-                        // Call ID is the same, use the active call
-                        activeCall
-                    }
-                } else {
-                    // There is no active call, create new call
-                    streamVideo.call(type = callId.type, id = callId.id)
-                }
-                if (activeCall != activeOrNewCall) {
-                    val joinResult = call.join(create = true)
-
-                    // Unable to join. Device is offline or other usually connection issue.
-                    if (joinResult is Result.Failure) {
-                        Log.e("CallActivity", "Call.join failed ${joinResult.value}")
-                        finish()
-                    }
-                }
-            }
-
-            setContent {
-                VideoTheme {
+        setContent {
+            VideoTheme {
+                callState?.let { call ->
                     val onCallAction: (CallAction) -> Unit = { callAction ->
                         when (callAction) {
                             is ToggleCamera -> call.camera.setEnabled(callAction.isEnabled)
@@ -112,6 +73,7 @@ class IncomingCallActivity : ComponentActivity() {
                                 call.leave()
                                 finish()
                             }
+
                             is DeclineCall -> {
                                 lifecycleScope.launch {
                                     call.reject()
@@ -119,12 +81,14 @@ class IncomingCallActivity : ComponentActivity() {
                                     finish()
                                 }
                             }
+
                             is AcceptCall -> {
                                 lifecycleScope.launch {
                                     call.accept()
                                     call.join()
                                 }
                             }
+
                             else -> Unit
                         }
                     }
@@ -149,6 +113,17 @@ class IncomingCallActivity : ComponentActivity() {
                         onCallAction = onCallAction,
                     )
                 }
+            }
+        }
+        // release the lock, turn on screen, and keep the device awake.
+        showWhenLockedAndTurnScreenOn()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                val callId = intent.streamCallId(NotificationHandler.INTENT_EXTRA_CALL_CID)!!
+                val streamVideo = StreamVideo.instance()
+                callState = streamVideo.call(callId.type, callId.id)
             }
         }
     }
