de051d1f4a198a4c6e2071f5ec3d292dc23fa1c3
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2023-12-14 14:37:50 +0100

    Remove sh from pre-commit script name

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ apply(from = "${rootDir}/scripts/publish-root.gradle")
 task("addPreCommitGitHookOnBuild") {
     println("⚈ ⚈ ⚈ Running Add Pre Commit Git Hook Script on Build ⚈ ⚈ ⚈")
     exec {
-        commandLine("cp", "./scripts/pre-commit.sh", "./.git/hooks")
+        commandLine("cp", "./scripts/pre-commit", "./.git/hooks")
     }
     println("✅ Added Pre Commit Git Hook Script.")
 }
