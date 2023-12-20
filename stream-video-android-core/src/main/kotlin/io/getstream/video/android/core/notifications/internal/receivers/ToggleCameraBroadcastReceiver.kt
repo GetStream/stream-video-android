f116109a4a31a752344237555d790c5da064789d
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2023-12-20 18:37:55 +0200

    Watch for activeCall and ringingCall value updates

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/notifications/internal/receivers/ToggleCameraBroadcastReceiver.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/notifications/internal/receivers/ToggleCameraBroadcastReceiver.kt
@@ -20,33 +20,67 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import io.getstream.log.taggedLogger
+import io.getstream.video.android.core.Call
 import io.getstream.video.android.core.StreamVideo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class ToggleCameraBroadcastReceiver(coroutineScope: CoroutineScope) : BroadcastReceiver() {
     private val logger by taggedLogger("ToggleCameraBroadcastReceiver")
     private val streamVideo = StreamVideo.instanceOrNull()
     private var call: Call? = null
     private var shouldEnableCameraAgain = false
-    private val logger by taggedLogger("ToggleCameraBroadcastReceiver")
+
+    init {
+        logger.d { "Init" }
+        logger.d { "Active call value: " + streamVideo?.state?.activeCall?.value?.id }
+        logger.d { "Ringing call value: " + streamVideo?.state?.ringingCall?.value?.id }
+
+        // TODO: active call should be set to ringing call automatically?
+        call = streamVideo?.state?.activeCall?.value ?: streamVideo?.state?.ringingCall?.value
+
+        coroutineScope.launch {
+            if (call == null) {
+                streamVideo?.state?.ringingCall?.collect {
+                    if (it != null) {
+                        call = it
+                        logger.d { "Ringing call changed to ${call?.id}" }
+                    }
+                }
+                streamVideo?.state?.activeCall?.collect {
+                    if (it != null) {
+                        call = it
+                        logger.d { "Active call changed to ${call?.id}" }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
             Intent.ACTION_SCREEN_ON -> {
-                logger.d { "Screen is on and locked." }
+                logger.d { "Screen is on and locked. Call: ${call?.id}" }
+                // TODO:
+                // For when the call screen is visible even if the screen is locked.
+                // Because of lockscreenVisibility = Notification.VISIBILITY_PUBLIC for channel?
+                // To reproduce: answer from locked screen the lock-unlock
+                // Problem: answer call while unlocked. Lock then turn screen on by moving phone -> camera is on. Turn screen on by tap or button -> camera is off.
+                if (shouldEnableCameraAgain) call?.camera?.enable()
             }
             Intent.ACTION_USER_PRESENT -> {
-                logger.d { "Screen is on and unlocked." }
-                if (shouldEnableCameraAgain) activeCall?.camera?.enable()
+                logger.d { "Screen is on and unlocked. Call: ${call?.id}" }
+                if (shouldEnableCameraAgain) call?.camera?.enable()
             }
             Intent.ACTION_SCREEN_OFF -> {
                 // This broadcast action actually means that the device is non-interactive.
                 // In a video call scenario, the only way to be non-interactive is when locking the phone manually.
-                activeCall?.camera.let { camera ->
+                call?.camera.let { camera ->
                     shouldEnableCameraAgain = camera?.isEnabled?.value ?: false
                     camera?.disable()
                 }
 
-                logger.d { "Screen is off. Should re-enable camera: $shouldEnableCameraAgain." }
+                logger.d { "Screen is off. Call: ${call?.id}. Should re-enable camera: $shouldEnableCameraAgain." }
             }
         }
     }
