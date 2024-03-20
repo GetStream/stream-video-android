b2aec294588b2781d16ab6f6796a638e0f1986df
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2024-03-20 14:06:03 +0100

    Update version to: 0.5.6

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,7 +6,7 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 0
     const val minorVersion = 5
-    const val patchVersion = 4
+    const val patchVersion = 6
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 17
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
