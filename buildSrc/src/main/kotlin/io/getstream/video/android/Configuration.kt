2788fe8ca2a16e72eaa377171f4c25aa1e94d4ac
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2024-04-23 09:50:58 +0200

    Prepare release 0.5.7 (#1076)

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,11 +6,11 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 0
     const val minorVersion = 5
-    const val patchVersion = 6
+    const val patchVersion = 7
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 20
+    const val versionCode = 21
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
-    const val streamVideoCallGooglePlayVersion = "1.0.8"
+    const val streamVideoCallGooglePlayVersion = "1.0.9"
     const val streamWebRtcVersionName = "1.1.1"
 }
