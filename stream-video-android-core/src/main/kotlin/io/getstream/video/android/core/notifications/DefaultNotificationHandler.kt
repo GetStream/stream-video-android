064458d434ea74c17806d336d5fcbbfb7c0735b3
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2024-07-15 10:55:27 +0300

    Create call setup notification channel

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/notifications/DefaultNotificationHandler.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/notifications/DefaultNotificationHandler.kt
@@ -148,13 +148,17 @@ public open class DefaultNotificationHandler(
 
     override fun getTemporaryNotification(): Notification? {
         val channelId = application.getString(
-            R.string.stream_video_ongoing_call_notification_channel_id,
+            R.string.stream_video_call_setup_notification_channel_id,
         )
 
         maybeCreateChannel(channelId, application) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                description =
-                    application.getString(R.string.stream_video_ongoing_call_notification_channel_description)
+                name = application.getString(
+                    R.string.stream_video_call_setup_notification_channel_title,
+                )
+                description = application.getString(
+                    R.string.stream_video_call_setup_notification_channel_description,
+                )
             }
         }
 
