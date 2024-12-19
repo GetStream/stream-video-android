ec422cc629d93f937d439f81da34a97ba652992a
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2024-12-19 13:32:37 +0100

    [AND-155] Connection timing (#1257)
    * Connection timing
    
    * Remove unused  fields from RTCSession

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
@@ -137,11 +137,13 @@ import stream.video.sfu.models.WebsocketReconnectStrategy
 import stream.video.sfu.signal.ICERestartRequest
 import stream.video.sfu.signal.ICERestartResponse
 import stream.video.sfu.signal.ICETrickleResponse
+import stream.video.sfu.signal.Reconnection
 import stream.video.sfu.signal.SendAnswerRequest
 import stream.video.sfu.signal.SendAnswerResponse
 import stream.video.sfu.signal.SendStatsRequest
 import stream.video.sfu.signal.SetPublisherRequest
 import stream.video.sfu.signal.SetPublisherResponse
+import stream.video.sfu.signal.Telemetry
 import stream.video.sfu.signal.TrackMuteState
 import stream.video.sfu.signal.TrackSubscriptionDetails
 import stream.video.sfu.signal.UpdateMuteStatesRequest
@@ -1631,6 +1633,7 @@ public class RtcSession internal constructor(
 
     internal suspend fun sendCallStats(report: CallStatsReport) {
         val result = wrapAPICall {
+            val now = System.currentTimeMillis()
             val androidThermalState =
                 safeCallWithDefault(AndroidThermalState.ANDROID_THERMAL_STATE_UNSPECIFIED) {
                     val thermalState = powerManager?.currentThermalStatus
@@ -1663,6 +1666,22 @@ public class RtcSession internal constructor(
                         thermal_state = androidThermalState,
                         is_power_saver_mode = powerSaving,
                     ),
+                    telemetry = safeCallWithDefault(null) {
+                        if (call.connectedAt != null || call.reconnectAt != null) {
+                            Telemetry(
+                                connection_time_seconds = call.connectedAt?.let { (now - it) / 1000 }
+                                    ?.toFloat(),
+                                reconnection = call.reconnectAt?.let {
+                                    Reconnection(
+                                        time_seconds = ((now - it.second) / 1000).toFloat(),
+                                        strategy = it.first,
+                                    )
+                                },
+                            )
+                        } else {
+                            null
+                        }
+                    },
                 ),
             )
         }
