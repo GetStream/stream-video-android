f81d7fa633910eb30862eb8e7ec150709eb5475a
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-01-14 17:37:08 +0100

    Formatting

--- stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/call/connection/PublisherTest.kt
+++ stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/call/connection/PublisherTest.kt
@@ -882,45 +882,44 @@ class PublisherTest {
     }
 
     fun buildEmptyEncodingParameters(
-            rid: String?,
-            active: Boolean,
-            maxFramerate: Int = 0,
-            scaleResolutionDownBy: Double = 1.0,
-            scalabilityMode: String? = null,
-            codec: RtpParameters.Codec? = null,
-            maxBitrate: Int,
-        ): RtpParameters {
-            val enc = RtpParameters.Encoding(
-                rid ?: "",
-                active,
-                1.0,
-            )
-            enc.maxFramerate = maxFramerate
-            enc.scaleResolutionDownBy = scaleResolutionDownBy
-            enc.scalabilityMode = scalabilityMode
-            enc.minBitrateBps = maxBitrate
-            enc.maxBitrateBps = maxBitrate
-            val constructor = RtpParameters::class.java.getDeclaredConstructor(
-                String::class.java,
-                RtpParameters.DegradationPreference::class.java,
-                RtpParameters.Rtcp::class.java,
-                MutableList::class.java, // for headerExtensions
-                MutableList::class.java, // for encodings
-                MutableList::class.java, // for codecs
-            )
-            constructor.isAccessible = true
+        rid: String?,
+        active: Boolean,
+        maxFramerate: Int = 0,
+        scaleResolutionDownBy: Double = 1.0,
+        scalabilityMode: String? = null,
+        codec: RtpParameters.Codec? = null,
+        maxBitrate: Int,
+    ): RtpParameters {
+        val enc = RtpParameters.Encoding(
+            rid ?: "",
+            active,
+            1.0,
+        )
+        enc.maxFramerate = maxFramerate
+        enc.scaleResolutionDownBy = scaleResolutionDownBy
+        enc.scalabilityMode = scalabilityMode
+        enc.minBitrateBps = maxBitrate
+        enc.maxBitrateBps = maxBitrate
+        val constructor = RtpParameters::class.java.getDeclaredConstructor(
+            String::class.java,
+            RtpParameters.DegradationPreference::class.java,
+            RtpParameters.Rtcp::class.java,
+            MutableList::class.java, // for headerExtensions
+            MutableList::class.java, // for encodings
+            MutableList::class.java, // for codecs
+        )
+        constructor.isAccessible = true
 
-            val rtpParameters = constructor.newInstance(
-                "fake-transaction-id",
-                null, // or a real DegradationPreference
-                null, // Rtcp object or null
-                emptyList<HeaderExtension>(), // headerExtensions
-                emptyList<RtpParameters.Encoding>(), // encodings
-                codec?.let { mutableListOf(codec) } ?: mutableListOf<Codec>(), // codecs
-            ) as RtpParameters
-            return rtpParameters
-        }
-
+        val rtpParameters = constructor.newInstance(
+            "fake-transaction-id",
+            null, // or a real DegradationPreference
+            null, // Rtcp object or null
+            emptyList<HeaderExtension>(), // headerExtensions
+            emptyList<RtpParameters.Encoding>(), // encodings
+            codec?.let { mutableListOf(codec) } ?: mutableListOf<Codec>(), // codecs
+        ) as RtpParameters
+        return rtpParameters
+    }
 
     /**
      * Uses reflection to create a [RtpParameters.Codec] instance, bypassing the private/package-private
