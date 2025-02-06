74380b663b0866914b449c1cdd83de5143ec1a43
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2025-02-06 16:44:05 +0200

    Add unit tests for TelecomHandler (almost final)

--- stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/telecom/TelecomHandlerTest.kt
+++ stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/telecom/TelecomHandlerTest.kt
@@ -16,16 +16,28 @@
 
 package io.getstream.video.android.core.telecom
 
+import android.app.NotificationManager
 import android.content.Context
+import android.telecom.DisconnectCause
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.telecom.CallControlScope
 import androidx.core.telecom.CallsManager
+import io.getstream.video.android.core.StreamVideo
+import io.getstream.video.android.core.StreamVideoClient
+import io.getstream.video.android.core.audio.StreamAudioDevice
 import io.getstream.video.android.core.notifications.internal.service.CallServiceConfig
 import io.getstream.video.android.core.sounds.CallSoundPlayer
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.mockkObject
+import io.mockk.runs
 import io.mockk.spyk
 import io.mockk.verify
 import kotlinx.coroutines.CoroutineScope
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -33,13 +45,15 @@ import kotlin.test.assertNotNull
 
 class TelecomHandlerTest {
     private lateinit var telecomHandler: TelecomHandler
-    private val context = mockk<Context>()
+    private val context = mockk<Context>(relaxed = true)
     private val callManager: CallsManager = mockk(relaxed = true)
     private val callSoundPlayer: CallSoundPlayer = mockk(relaxed = true)
     private val streamCall: StreamCall = mockk(relaxed = true)
     private val callConfig: CallServiceConfig = mockk(relaxed = true)
     private val coroutineScope: CoroutineScope = mockk(relaxed = true)
     private lateinit var telecomCall: TelecomCall
+    private val streamVideo: StreamVideoClient = mockk(relaxed = true)
+    private val notificationManager = mockk<NotificationManager>(relaxed = true)
 
     @Before
     fun setUp() {
@@ -51,16 +65,32 @@ class TelecomHandlerTest {
             ),
             recordPrivateCalls = true,
         )
+        every { telecomHandler["hasNotificationsPermission"]() } returns true
 
         every { streamCall.cid } returns "default:123"
 
         telecomCall = spyk(
             TelecomCall(streamCall = streamCall, config = callConfig, parentScope = coroutineScope),
         )
-    }
 
-    @After
-    fun tearDown() {
+        mockkObject(StreamVideo)
+        every { StreamVideo.instanceOrNull() } returns streamVideo
+
+        coEvery {
+            callManager.addCall(
+                telecomCall.attributes,
+                any<suspend (Int) -> Unit>(),
+                any<suspend (DisconnectCause) -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<CallControlScope.() -> Unit>(),
+            )
+        } just runs
+
+        every { context.getSystemService(Context.NOTIFICATION_SERVICE) } returns notificationManager
+        mockkConstructor(NotificationManagerCompat::class)
+        every { anyConstructed<NotificationManagerCompat>().notify(any(), any()) } just runs
+        every { anyConstructed<NotificationManagerCompat>().cancel(any()) } just runs
     }
 
     @Test
@@ -111,7 +141,7 @@ class TelecomHandlerTest {
 
         telecomHandler.changeCallState(streamCall, newState)
 
-        verify(exactly = 1) { telecomCall.state }
+        verify(exactly = 1) { telecomCall.state = newState }
         assertEquals(newState, telecomCall.state)
         assertEquals(newState, calls[streamCall.cid]?.state)
     }
@@ -133,8 +163,10 @@ class TelecomHandlerTest {
 
         telecomHandler.changeCallState(streamCall, newState)
 
-        verify(exactly = 1) { telecomHandler["postNotification"](telecomCall) }
-        verify(exactly = 1) { telecomHandler["hasNotificationsPermission"]() }
+        verify(exactly = 1) {
+            telecomHandler["postNotification"](telecomCall)
+            telecomHandler["hasNotificationsPermission"]()
+        }
     }
 
     @Test
@@ -155,5 +187,139 @@ class TelecomHandlerTest {
         telecomHandler.changeCallState(streamCall, newState)
 
         verify(exactly = 0) { telecomHandler["postNotification"](any<TelecomCall>()) }
+    }
+
+    @Test
+    fun changeCallState_fromIdleToIncoming_addsCallToTelecom() {
+        val calls = getPrivateCallsMap()
+        calls[streamCall.cid] = telecomCall.apply { state = TelecomCallState.IDLE }
+        val newState = TelecomCallState.INCOMING
+
+        telecomHandler.changeCallState(streamCall, newState)
+
+        coVerify(exactly = 1) {
+            callManager.addCall(
+                telecomCall.attributes,
+                any<suspend (Int) -> Unit>(),
+                any<suspend (DisconnectCause) -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<CallControlScope.() -> Unit>(),
+            )
+        }
+    }
+
+    @Test
+    fun changeCallState_fromIdleToOutgoing_addsCallToTelecom() {
+        val calls = getPrivateCallsMap()
+        calls[streamCall.cid] = telecomCall.apply { state = TelecomCallState.IDLE }
+        val newState = TelecomCallState.OUTGOING
+
+        telecomHandler.changeCallState(streamCall, newState)
+
+        coVerify(exactly = 1) {
+            callManager.addCall(
+                telecomCall.attributes,
+                any<suspend (Int) -> Unit>(),
+                any<suspend (DisconnectCause) -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<CallControlScope.() -> Unit>(),
+            )
+        }
+    }
+
+    @Test
+    fun changeCallState_fromIncomingToOngoing_doesNotAddCallToTelecom() {
+        val calls = getPrivateCallsMap()
+        calls[streamCall.cid] = telecomCall.apply { state = TelecomCallState.INCOMING }
+        val newState = TelecomCallState.ONGOING
+
+        telecomHandler.changeCallState(streamCall, newState)
+
+        coVerify(exactly = 0) {
+            callManager.addCall(
+                telecomCall.attributes,
+                any<suspend (Int) -> Unit>(),
+                any<suspend (DisconnectCause) -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<CallControlScope.() -> Unit>(),
+            )
+        }
+    }
+
+    @Test
+    fun changeCallState_fromOutgoingToOngoing_doesNotAddCallToTelecom() {
+        val calls = getPrivateCallsMap()
+        calls[streamCall.cid] = telecomCall.apply { state = TelecomCallState.OUTGOING }
+        val newState = TelecomCallState.ONGOING
+
+        telecomHandler.changeCallState(streamCall, newState)
+
+        coVerify(exactly = 0) {
+            callManager.addCall(
+                telecomCall.attributes,
+                any<suspend (Int) -> Unit>(),
+                any<suspend (DisconnectCause) -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<suspend () -> Unit>(),
+                any<CallControlScope.() -> Unit>(),
+            )
+        }
+    }
+
+//    @Test
+//    fun changeCallState_fromIdleToIncoming_playsCallSound() {
+//        val calls = getPrivateCallsMap()
+//        calls[streamCall.cid] = telecomCall.apply { state = TelecomCallState.IDLE }
+//        val newState = TelecomCallState.INCOMING
+//        every { telecomHandler["hasNotificationsPermission"]() } returns true
+//
+//        telecomHandler.changeCallState(streamCall, newState)
+//
+//        assert(telecomCall.state == newState)
+//        verify(exactly = 1) { telecomHandler["postNotification"](any<TelecomCall>()) }
+//        verify(exactly = 1) { telecomHandler["hasNotificationsPermission"]() }
+//        verify(exactly = 1) { streamVideo.getRingingCallNotification(
+//            any<RingingState>(),
+//            any<StreamCallId>(),
+//            any<String>(),
+//            any<Boolean>(),
+//        ) }
+//        verify(exactly = 1) { callSoundPlayer.playCallSound(any<Uri>()) }
+//    }
+
+    @Test
+    fun setDeviceListener_existingCall_setsDeviceListenerAndSendsInitialDevices() {
+        val calls = getPrivateCallsMap()
+        calls[streamCall.cid] = telecomCall
+        val deviceListener: DeviceListener = mockk(relaxed = true)
+        every { telecomCall.deviceListener = any() } just runs // Stub setter
+
+        telecomHandler.setDeviceListener(streamCall, deviceListener)
+
+        verify(exactly = 1) {
+            telecomCall.deviceListener = deviceListener
+            telecomHandler["sendInitialDevices"](deviceListener)
+            callManager.getAvailableStartingCallEndpoints()
+            deviceListener.invoke(any<List<StreamAudioDevice>>(), any<StreamAudioDevice>())
+        }
+    }
+
+    @Test
+    fun setDeviceListener_inexistentCall_doesNotSetDeviceListenerButSendsInitialDevices() {
+        val deviceListener: DeviceListener = mockk(relaxed = true)
+
+        telecomHandler.setDeviceListener(streamCall, deviceListener)
+
+        verify(exactly = 0) {
+            telecomCall.deviceListener = any<DeviceListener>()
+        }
+        verify(exactly = 1) {
+            telecomHandler["sendInitialDevices"](any<DeviceListener>())
+            callManager.getAvailableStartingCallEndpoints()
+            deviceListener.invoke(any<List<StreamAudioDevice>>(), any<StreamAudioDevice>())
+        }
     }
 }
