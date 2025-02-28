ed63f861accd4c061e9377e875b445fbfa844e34
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2025-02-28 16:24:05 +0200

    [AND-345] Fix: fields not updated by queryCalls (#1303)
    * Update ownCapabilities
    
    * Add missing capabilities to OwnCapability
    
    * API dump
    
    ---------
    
    Co-authored-by: Aleksandar Apostolov <apostolov.alexandar@gmail.com>

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/CallState.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/CallState.kt
@@ -1316,6 +1316,7 @@ public class CallState(
 
     fun updateFromResponse(callData: CallStateResponseFields) {
         updateFromResponse(callData.call)
+        _ownCapabilities.value = callData.ownCapabilities
         updateFromResponse(callData.members)
     }
 
