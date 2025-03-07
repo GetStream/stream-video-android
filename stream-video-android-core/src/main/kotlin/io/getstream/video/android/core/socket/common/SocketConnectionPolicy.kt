b8e1bebca5028c81d30503e0e8744b8d11fa8101
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2025-03-07 15:54:16 +0200

    [AND-348] Fix socket behavior when app is minimized (#1306)
    * Refactor socket lifecycle connect/disconnect
    
    * Add unit tests
    
    ---------
    
    Co-authored-by: Aleksandar Apostolov <apostolov.alexandar@gmail.com>

---
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/socket/common/SocketConnectionPolicy.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.android.core.socket.common
+
+import io.getstream.video.android.core.StreamVideo
+import io.getstream.video.android.core.socket.coordinator.state.VideoSocketState
+import kotlinx.coroutines.flow.StateFlow
+
+interface SocketConnectionPolicy {
+    fun shouldConnect(): Boolean
+    fun shouldDisconnect(): Boolean
+}
+
+class CallAwareConnectionPolicy(private val streamVideo: StateFlow<StreamVideo?>) : SocketConnectionPolicy {
+    override fun shouldConnect(): Boolean = true
+
+    override fun shouldDisconnect(): Boolean {
+        val streamVideo = streamVideo.value
+
+        return streamVideo == null || !streamVideo.state.hasActiveOrRingingCall()
+    }
+}
+
+class SocketStateConnectionPolicy(private val currentStateFlow: StateFlow<VideoSocketState>) : SocketConnectionPolicy {
+    override fun shouldConnect(): Boolean {
+        val state = currentStateFlow.value
+
+        return state is VideoSocketState.Disconnected.DisconnectedPermanently ||
+            state is VideoSocketState.Disconnected.DisconnectedTemporarily ||
+            state is VideoSocketState.Disconnected.NetworkDisconnected ||
+            state is VideoSocketState.Disconnected.DisconnectedByRequest ||
+            state is VideoSocketState.Disconnected.Stopped
+    }
+
+    override fun shouldDisconnect(): Boolean {
+        val state = currentStateFlow.value
+
+        return state is VideoSocketState.Connected ||
+            state is VideoSocketState.Connecting ||
+            state is VideoSocketState.Disconnected.DisconnectedTemporarily
+    }
+}
