13b5c927a3b6aff9f621b95dca58180996b2e3ff
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-05-06 13:18:11 +0200

    Update version to 1.4.7

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -6,11 +6,11 @@ object Configuration {
     const val minSdk = 24
     const val majorVersion = 1
     const val minorVersion = 4
-    const val patchVersion = 6
+    const val patchVersion = 7
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 57
+    const val versionCode = 58
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
-    const val streamVideoCallGooglePlayVersion = "1.4.6"
+    const val streamVideoCallGooglePlayVersion = "1.4.7"
     const val streamWebRtcVersionName = "1.3.6"
 }
