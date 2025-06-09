b46e54b68f7f59b3c261437a3a283b5b28086d91
Jc Miñarro <josecarlos.minarro@gmail.com>
2025-06-09 11:57:05 +0200

    Create VideoApi

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/model/CallTranscription.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/model/CallTranscription.kt
@@ -3,7 +3,7 @@ package io.getstream.video.android.core.model
 import org.threeten.bp.OffsetDateTime
 
 public data class CallTranscription(
-    val endTime: org.threeten.bp.OffsetDateTime,
+    val endTime: OffsetDateTime,
     val filename: String,
     val startTime: OffsetDateTime,
     val url: String,
