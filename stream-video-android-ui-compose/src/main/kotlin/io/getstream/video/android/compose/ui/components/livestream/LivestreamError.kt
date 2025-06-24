0c12c08798b8ec026dfa4d47f202de779a9d1692
rahullohra <tgunix@gmail.com>
2025-06-24 16:34:42 +0530

    feat: Update Livestream Ui

--- stream-video-android-ui-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/livestream/LivestreamError.kt
+++ stream-video-android-ui-compose/src/main/kotlin/io/getstream/video/android/compose/ui/components/livestream/LivestreamError.kt
@@ -17,16 +17,25 @@
 package io.getstream.video.android.compose.ui.components.livestream
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.getstream.video.android.compose.theme.VideoTheme
 import io.getstream.video.android.compose.ui.components.base.StreamButton
+import io.getstream.video.android.compose.ui.components.base.styling.ButtonStyles
+import io.getstream.video.android.compose.ui.components.base.styling.StyleSize
 import io.getstream.video.android.core.Call
 import io.getstream.video.android.ui.common.R
 
@@ -37,16 +46,27 @@ internal fun LivestreamErrorUi(call: Call, onRetryJoin: () -> Unit) {
             .fillMaxSize()
             .background(color = VideoTheme.colors.baseSheetPrimary),
     ) {
-        Column {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
             Text(
-//                modifier = Modifier.align(Alignment.Center),
                 text = stringResource(
                     id = R.string.stream_video_livestreaming_error_joining,
                 ),
                 fontSize = 18.sp,
                 color = VideoTheme.colors.basePrimary,
+                textAlign = TextAlign.Center,
             )
-            StreamButton(text = "Retry", onClick = onRetryJoin)
+            Spacer(Modifier.height(12.dp))
+            StreamButton(
+                text = "Retry",
+                onClick = onRetryJoin,
+                style = ButtonStyles.secondaryButtonStyle(size = StyleSize.M),
+            )
         }
     }
 }
