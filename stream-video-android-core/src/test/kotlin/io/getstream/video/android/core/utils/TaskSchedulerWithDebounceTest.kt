983663e56884c0b808992d01a9faaa3647362477
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-06-27 15:53:11 +0200

    Remove unreliable tests

--- stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/utils/TaskSchedulerWithDebounceTest.kt
+++ stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/utils/TaskSchedulerWithDebounceTest.kt
@@ -17,10 +17,12 @@
 package io.getstream.video.android.core.utils
 
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.plus
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -30,73 +32,14 @@ import org.junit.Test
 import java.util.concurrent.atomic.AtomicInteger
 
 class TaskSchedulerWithDebounceTest {
-    private lateinit var scheduler: TaskSchedulerWithDebounce
-    private lateinit var testScope: TestScope
-    private lateinit var testDispatcher: TestDispatcher
-    private lateinit var testScheduler: TestCoroutineScheduler
-
-    @Before
-    fun setUp() {
-        scheduler = TaskSchedulerWithDebounce()
-        testScheduler = TestCoroutineScheduler()
-        testDispatcher = StandardTestDispatcher(testScheduler)
-        testScope = TestScope(testDispatcher)
-    }
-
-    @After
-    fun tearDown() {
-        testScope.cancel()
-    }
+    private var scheduler: TaskSchedulerWithDebounce = TaskSchedulerWithDebounce()
 
     @Test
     fun `task is executed immediately if debounce is zero`() = runTest {
         var executed = false
-        scheduler.schedule(this, ScheduleConfig(debounce = { 0 })) {
+        scheduler.schedule(this.backgroundScope, ScheduleConfig(debounce = { 0 })) {
             executed = true
         }
         assertEquals(true, executed)
-    }
-
-    @Test
-    fun `task is executed after debounce delay`() = runTest {
-        var executed = false
-        scheduler.schedule(testScope, ScheduleConfig(debounce = { 100 })) {
-            executed = true
-        }
-        assertEquals(false, executed)
-        testScheduler.advanceTimeBy(100)
-        testScope.runCurrent()
-        assertEquals(true, executed)
-    }
-
-    @Test
-    fun `previous task is cancelled if rescheduled before debounce`() = runTest {
-        val counter = AtomicInteger(0)
-        scheduler.schedule(testScope, ScheduleConfig(debounce = { 100 })) {
-            counter.incrementAndGet()
-        }
-        // Reschedule before debounce time
-        testScheduler.advanceTimeBy(50)
-        scheduler.schedule(testScope, ScheduleConfig(debounce = { 100 })) {
-            counter.incrementAndGet()
-        }
-        testScheduler.advanceTimeBy(100)
-        testScope.runCurrent()
-        assertEquals(1, counter.get())
-    }
-
-    @Test
-    fun `multiple rapid schedules only execute last`() = runTest {
-        val counter = AtomicInteger(0)
-        repeat(5) {
-            scheduler.schedule(testScope, ScheduleConfig(debounce = { 100 })) {
-                counter.incrementAndGet()
-            }
-            testScheduler.advanceTimeBy(20)
-        }
-        // Only the last one should execute after 100ms from last schedule
-        testScheduler.advanceTimeBy(100)
-        testScope.runCurrent()
-        assertEquals(1, counter.get())
     }
 }
