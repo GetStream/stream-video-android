bfead3cdfc4f34fe37df35303040247e9ecbc0bc
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-07-09 16:38:54 +0200

    Spotless & ApiDump

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/RtcSession.kt
@@ -418,7 +418,7 @@ public class RtcSession internal constructor(
 
                     is SfuSocketState.Disconnected.WebSocketEventLost -> {
                         _peerConnectionStates.value.let {
-                            if(publisher?.isHealthy() == true && subscriber?.isHealthy() == true) {
+                            if (publisher?.isHealthy() == true && subscriber?.isHealthy() == true) {
                                 call.fastReconnect()
                             } else {
                                 call.rejoin()
