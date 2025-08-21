6bf43c52a0518d5e4b458df293df7329559baf88
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-08-21 13:26:10 +0200

    Add base snapshot version name

--- buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/video/android/Configuration.kt
@@ -9,6 +9,7 @@ object Configuration {
     const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val versionCode = 57
+    const val snapshotBasedVersionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
     const val streamVideoCallGooglePlayVersion = versionName
