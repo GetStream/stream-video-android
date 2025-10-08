e07309c7990a5f8d5e4ec47f87b0e42c7f139d57
rahullohra <tgunix@gmail.com>
2025-10-08 18:32:09 +0530

    1. skip Generate open api code from spotless & ktlint

--- build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -14,7 +14,7 @@ class SpotlessConventionPlugin : Plugin<Project> {
                     target("**/*.kt")
                     targetExclude(
                             "**/build/**/*.kt",                  // Build directory
-                            "**/org/openapitools/client/**/*.kt" // OpenAPI generated code
+                            "**/generated/**/*.kt" // OpenAPI generated code
                     )
                     ktlint()
                         .editorConfigOverride(
