cd6b592a74ecba53e725999a1f7d5e56a45018d5
Pratim Mallick <34009202+PratimMallick@users.noreply.github.com>
2025-10-14 13:48:26 +0200

    Upgrade to webrtc m137 and remove stream-webrtc-android-ui dep (#1543)
    * Create stateFlow objects only when first accessed, reducing memory usage for unused participants
    
    * Remove the tight coupling of ParticipantState to the Call object
    
    * 1. Use concurrent Hashmap instead of SortMap for better performance.
    2. No need to copy the Map everytime as updates to concurrent map can be made in thread safe way
    3. Keep an internal mutable map and use a shallow copy of Hashmap for emitting externally - reduces unce
    
    * Use SerialProcessor based by a single thread executor, so that we can guarantee that all webrtc calls are executed from the same thread in a serial manner.
    
    * Added extra logging to print the order of jobs submitted to serial processor
    
    * Api dump updated
    
    * Fixed formmatting
    
    * Fixed tests
    
    * Removed bang operator
    
    * Trying to fix reconnect issue
    
    * - Upgraded to webrtc m137
    - Removed dependency of stream.webrtc.ui
    
    * chore
    
    * Updated to webrtc m137
    
    * Apply spotless changes
    
    * api dump applied
    
    * Remove lazy loading in ParticipantState that was introduced in merging.
    Remove scalability mode checks in Publisher.kt
    Make VideoCodecScalabilityChecker internal
    
    * moved VideoTextureViewRenderer to its original package name so that there is no breaking changes in the public api
    
    * Added consumer proguard rules to webrtc
    
    ---------
    
    Co-authored-by: Aleksandar Apostolov <apostolov.alexandar@gmail.com>

---
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/call/connection/utils/VideoCodecScalabilityChecker.kt
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.android.core.call.connection.utils
+
+import android.util.Log
+import org.webrtc.DefaultVideoEncoderFactory
+import org.webrtc.EglBase
+import org.webrtc.HardwareVideoEncoderFactory
+import org.webrtc.ResolutionAdjustment
+import org.webrtc.SimulcastAlignedVideoEncoderFactory
+import org.webrtc.SoftwareVideoEncoderFactory
+import org.webrtc.VideoCodecInfo
+import org.webrtc.VideoEncoderFactory
+
+/**
+ * Utility class to check video codec scalability modes on Android devices
+ * This helps debug device-specific codec capabilities
+ */
+internal object VideoCodecScalabilityChecker {
+    private const val TAG = "VideoCodecScalabilityChecker"
+
+    /**
+     * Check and log all supported video codecs and their scalability modes
+     */
+    fun checkVideoCodecScalabilityModes() {
+        Log.i(TAG, "=== Starting Video Codec Scalability Mode Check ===")
+
+        // Create different encoder factories to test
+        val factories = arrayOf<VideoEncoderFactory?>(
+            SoftwareVideoEncoderFactory(),
+//            HardwareVideoEncoderFactory(null, false, true),
+            SimulcastAlignedVideoEncoderFactory(
+                EglBase.create().eglBaseContext,
+                true,
+                true,
+                ResolutionAdjustment.MULTIPLE_OF_16,
+            ),
+            DefaultVideoEncoderFactory(null, false, true),
+        )
+
+        val factoryNames = arrayOf<String?>(
+            "SoftwareVideoEncoderFactory",
+            "HardwareVideoEncoderFactory",
+            "DefaultVideoEncoderFactory",
+        )
+
+        for (i in factories.indices) {
+            Log.i(TAG, "\n--- " + factoryNames[i] + " ---")
+            checkCodecFactory(factories[i]!!, factoryNames[i])
+        }
+
+        Log.i(TAG, "=== Video Codec Scalability Mode Check Complete ===")
+    }
+
+    /**
+     * Check a specific video encoder factory
+     */
+    private fun checkCodecFactory(factory: VideoEncoderFactory, factoryName: String?) {
+        try {
+            val supportedCodecs = factory.supportedCodecs
+            Log.i(TAG, factoryName + " supports " + supportedCodecs.size + " codecs:")
+
+            for (codecInfo in supportedCodecs) {
+                logCodecInfo(codecInfo, factoryName)
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Error checking " + factoryName + ": " + e.message)
+        }
+    }
+
+    /**
+     * Log detailed information about a single codec
+     */
+    private fun logCodecInfo(codecInfo: VideoCodecInfo, factoryName: String?) {
+        val log = StringBuilder()
+        log.append("Codec: ").append(codecInfo.name)
+        log.append(" | Params: ").append(codecInfo.params)
+
+        // Check scalability modes
+        val scalabilityModes = codecInfo.scalabilityModes
+        if (scalabilityModes == null) {
+            log.append(" | ScalabilityModes: NULL")
+        } else if (scalabilityModes.isEmpty()) {
+            log.append(" | ScalabilityModes: EMPTY LIST")
+        } else {
+            log.append(" | ScalabilityModes: [")
+            for (i in scalabilityModes.indices) {
+                if (i > 0) log.append(", ")
+                log.append(scalabilityModes.get(i))
+            }
+            log.append("]")
+        }
+
+        Log.i(TAG, log.toString())
+    }
+
+    /**
+     * Check if a specific scalability mode is supported for a codec
+     */
+    fun isScalabilityModeSupported(codecName: String?, scalabilityMode: String?): Boolean {
+        val factory: VideoEncoderFactory = DefaultVideoEncoderFactory(null, false, true)
+        val supportedCodecs = factory.getSupportedCodecs()
+
+        for (codecInfo in supportedCodecs) {
+            if (codecInfo.name.equals(codecName, ignoreCase = true)) {
+                val scalabilityModes = codecInfo.scalabilityModes
+                if (scalabilityModes != null) {
+                    return scalabilityModes.contains(scalabilityMode)
+                }
+            }
+        }
+        return false
+    }
+
+    /**
+     * Get all supported scalability modes for a specific codec
+     */
+    fun getSupportedScalabilityModes(codecName: String?): MutableList<String?> {
+        val factory: VideoEncoderFactory = DefaultVideoEncoderFactory(null, false, true)
+        val supportedCodecs = factory.getSupportedCodecs()
+
+        for (codecInfo in supportedCodecs) {
+            if (codecInfo.name.equals(codecName, ignoreCase = true)) {
+                val scalabilityModes = codecInfo.scalabilityModes
+                return if (scalabilityModes != null) {
+                    ArrayList<String?>(
+                        scalabilityModes,
+                    )
+                } else {
+                    ArrayList<String?>()
+                }
+            }
+        }
+        return ArrayList<String?>()
+    }
+
+    /**
+     * Compare codec capabilities between different factories
+     */
+    fun compareCodecCapabilities() {
+        Log.i(TAG, "=== Comparing Codec Capabilities ===")
+
+        val softwareFactory: VideoEncoderFactory = SoftwareVideoEncoderFactory()
+        val hardwareFactory: VideoEncoderFactory = HardwareVideoEncoderFactory(null, false, true)
+
+        val softwareCodecs = softwareFactory.getSupportedCodecs()
+        val hardwareCodecs = hardwareFactory.getSupportedCodecs()
+
+        Log.i(TAG, "Software codecs: " + softwareCodecs.size)
+        Log.i(TAG, "Hardware codecs: " + hardwareCodecs.size)
+
+        // Find common codecs and compare their scalability modes
+        for (swCodec in softwareCodecs) {
+            for (hwCodec in hardwareCodecs) {
+                if (swCodec.name.equals(hwCodec.name, ignoreCase = true)) {
+                    Log.i(TAG, "\nComparing codec: " + swCodec.name)
+                    Log.i(
+                        TAG,
+                        "  Software scalability modes: " +
+                            (if (swCodec.scalabilityModes != null) swCodec.scalabilityModes else "NULL"),
+                    )
+                    Log.i(
+                        TAG,
+                        "  Hardware scalability modes: " +
+                            (if (hwCodec.scalabilityModes != null) hwCodec.scalabilityModes else "NULL"),
+                    )
+
+                    // Check if they're different
+                    val different = swCodec.scalabilityModes != hwCodec.scalabilityModes
+                    if (different) {
+                        Log.w(TAG, "  *** DIFFERENCE DETECTED for " + swCodec.name + " ***")
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    /**
+     * Test specific scalability modes that are commonly used
+     */
+    fun testCommonScalabilityModes() {
+        Log.i(TAG, "=== Testing Common Scalability Modes ===")
+
+        val commonModes = arrayOf<String?>(
+            "L1T1", "L1T2", "L1T3", "L2T1", "L2T2", "L2T3",
+            "L3T1", "L3T2", "L3T3", "S2T1", "S2T2", "S2T3",
+        )
+
+        val commonCodecs = arrayOf<String?>("VP8", "VP9", "H264", "AV1", "H265")
+
+        val factory: VideoEncoderFactory = DefaultVideoEncoderFactory(null, false, true)
+        val supportedCodecs = factory.getSupportedCodecs()
+
+        for (codecName in commonCodecs) {
+            Log.i(TAG, "\nTesting codec: " + codecName)
+            val supportedModes = getSupportedScalabilityModes(codecName)
+
+            if (supportedModes.isEmpty()) {
+                Log.w(TAG, "  No scalability modes supported for " + codecName)
+            } else {
+                Log.i(TAG, "  Supported modes: " + supportedModes)
+
+                for (mode in commonModes) {
+                    val supported = supportedModes.contains(mode)
+                    Log.i(
+                        TAG,
+                        "    " + mode + ": " + (if (supported) "SUPPORTED" else "NOT SUPPORTED"),
+                    )
+                }
+            }
+        }
+    }
+}
