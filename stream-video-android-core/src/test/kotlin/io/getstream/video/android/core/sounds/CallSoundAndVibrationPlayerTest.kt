32a5abeb2c0763ce912fdc2ee244ec01e88d81d1
Aleksandar Apostolov <apostolov.alexandar@gmail.com>
2025-10-20 16:52:45 +0200

    Spotless & apiDump

--- stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/sounds/CallSoundAndVibrationPlayerTest.kt
+++ stream-video-android-core/src/test/kotlin/io/getstream/video/android/core/sounds/CallSoundAndVibrationPlayerTest.kt
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.video.android.core.sounds
 
 import android.content.Context
-import android.os.Looper
 import android.os.Vibrator
 import io.mockk.clearAllMocks
 import io.mockk.clearMocks
@@ -11,8 +26,6 @@ import io.mockk.verify
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
 class CallSoundAndVibrationPlayerTest {
 
