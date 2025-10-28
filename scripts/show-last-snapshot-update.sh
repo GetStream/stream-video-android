7183c9742dde4d718cd47f2b821ad5ddbe36ed90
Rahul Kumar Lohra <tgunix@gmail.com>
2025-10-28 17:03:30 +0530

    Fix CI to display the correct Sonatype-assigned snapshot version (#1557)
    * update snapshot url
    
    * print version name
    
    * remove timestamp
    
    * refactor timestamp with MMddHHmm
    
    * correct the logic to render snapshot version on ci
    
    * refactor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * fix snapshot version
    
    ---------
    
    Co-authored-by: Aleksandar Apostolov <apostolov.alexandar@gmail.com>

--- scripts/show-last-snapshot-update.sh
+++ scripts/show-last-snapshot-update.sh
@@ -5,13 +5,12 @@
     # Extract groupId, artifactId, and snapshot version from the artifact string
     groupId=$(echo $artifact | cut -d: -f1)
     artifactId=$(echo $artifact | cut -d: -f2)
-    snapshotVersion=$(echo $artifact | cut -d: -f3)
 
     # Format the URL for the maven-metadata.xml file in the Nexus repository
-    url="https://oss.sonatype.org/content/repositories/snapshots/$(echo $groupId | tr '.' '/')/$artifactId/$snapshotVersion/maven-metadata.xml"
+    url="https://central.sonatype.com/repository/maven-snapshots/io/getstream/stream-video-android-bom/maven-metadata.xml"
 
     # Fetch the maven-metadata.xml using curl and extract the latest release version using sed
-    latest_version=$(curl -s "$url" | sed -n 's|.*<value>\(.*\)</value>.*|\1|p' | head -n 1)
+    latest_version=$(curl -s "$url" | sed -n 's/.*<latest>\(.*\)<\/latest>.*/\1/p')
 
     # Print the result with the latest stable version
     if [ -n "$latest_version" ]; then
