17b1dbaa69b12ff251e7fd7bb7283983cc28bf02
Rahul Kumar Lohra <tgunix@gmail.com>
2025-10-29 19:45:24 +0530

    Introduce Telecom support for the `VideoSDK` (#1536)
    * phase 1: code refactor - non telecom related
    
    * phase 2: integrate jetpack telecom
    
    * phase 3: correct leaving telecom call logic
    
    * phase 4: add video call support
    
    * refactor permission logic
    
    * refactor permissions from manifest
    
    * update androidx-telecom
    
    * Mark classes as internal/private
    
    * Mark debugging apis as internal
    
    * Mark apis as internal
    
    * Mark apis as internal
    
    * Remove unused internal apis from CallService
    
    * Remove comments
    
    * Remove dependencies
    
    * fix imports
    
    * fix paparazi tests
    
    * reaarange files in correct packages
    
    * refactor
    
    * refactor
    
    * fix double activation of telecom
    
    * improve Unit tests
    
    * refactor api
    
    * refactor api
    
    * telecom will be enabled from callServiceConfigRegistry
    
    * refactor
    
    * improve unit tests
    
    * fix merge conflicts
    
    * Merge
    
    * new api dump
    
    ---------
    
    Co-authored-by: Aleksandar Apostolov <apostolov.alexandar@gmail.com>

---
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/notifications/IncomingNotificationData.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-video-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.video.android.core.notifications
+
+import android.app.PendingIntent
+
+internal data class IncomingNotificationData(val pendingIntentMap: Map<IncomingNotificationAction, PendingIntent>)
+
+internal sealed class IncomingNotificationAction {
+    data object Accept : IncomingNotificationAction()
+    data object Reject : IncomingNotificationAction()
+}
