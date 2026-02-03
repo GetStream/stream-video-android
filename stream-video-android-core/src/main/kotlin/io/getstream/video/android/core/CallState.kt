f788990faeeaea053685cf8c31a3efc621b64fc0
rahullohra <tgunix@gmail.com>
2026-02-04 00:01:46 +0530

    chore: checkpoint 12.

--- stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/CallState.kt
+++ stream-video-android-core/src/main/kotlin/io/getstream/video/android/core/CallState.kt
@@ -147,7 +147,6 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.transform
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
