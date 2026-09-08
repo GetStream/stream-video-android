dc31b8eeadefe72a43f9cb9a74244c2cabf960c0
rahullohra <tgunix@gmail.com>
2026-09-08 12:26:20 +0530

    Spotless

--- demo-app/src/androidTestE2etestingDebug/kotlin/io/getstream/video/android/robots/UserRobotCallAsserts.kt
+++ demo-app/src/androidTestE2etestingDebug/kotlin/io/getstream/video/android/robots/UserRobotCallAsserts.kt
@@ -295,7 +295,7 @@ fun UserRobot.assertOutgoingCall(audioOnly: Boolean = true, isDisplayed: Boolean
         assertTrue(
             "Microphone",
             RingPage.microphoneEnabledToggle.waitDisplayed() ||
-                    RingPage.microphoneDisabledToggle.waitDisplayed(),
+                RingPage.microphoneDisabledToggle.waitDisplayed(),
         )
         if (audioOnly) {
             assertFalse("Camera enabled toggle", RingPage.cameraEnabledToggle.isDisplayed())
@@ -304,7 +304,7 @@ fun UserRobot.assertOutgoingCall(audioOnly: Boolean = true, isDisplayed: Boolean
             assertTrue(
                 "Camera",
                 RingPage.cameraEnabledToggle.waitDisplayed() ||
-                        RingPage.cameraDisabledToggle.waitDisplayed(),
+                    RingPage.cameraDisabledToggle.waitDisplayed(),
             )
         }
     } else {
