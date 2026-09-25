79bfe340a4e0f9bd024a57609d9550ae9413ecf5
rahullohra <tgunix@gmail.com>
2026-09-25 13:42:57 +0530

    use getstream audioswitch

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,6 @@ pluginManagement {
         google()
         mavenCentral()
         mavenLocal()
-        maven(url = "https://central.sonatype.com/repository/maven-snapshots/") {
-            content {
-                includeGroupByRegex("io\\.getstream.*")
-            }
-            mavenContent {
-                snapshotsOnly()
-            }
-        }
     }
 }
 dependencyResolutionManagement {
@@ -25,14 +17,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://plugins.gradle.org/m2/")
-        maven(url = "https://central.sonatype.com/repository/maven-snapshots/") {
-            content {
-                includeGroupByRegex("io\\.getstream.*")
-            }
-            mavenContent {
-                snapshotsOnly()
-            }
-        }
     }
 }
 
